5f220b7a6afb8de157ee98a664e083706627eb3b
Leonora <leonoraszallies@gmail.com>
2026-06-19 13:56:40 +1200

    a

---
+++ pages/home.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import os
+
+class HomePage:
+    root = tk.Tk()
+    root.title("HomePage")
+    root.geometry("324x720")
+
+    label = tk.Label(root, text = "Ready to organize your week?", font=('Arial', 16))
+    label.pack()
+
+    def openRecipes():
+        os.recipes
+    button=tk.Button(root, text = "Recipe Book", command = openRecipes)
+    button.pack()
+
+
+
+    root.mainloop()
