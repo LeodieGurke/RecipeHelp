74b1adf8c3f0b6d2259166b4ec283a5c343b37fc
Leonora <leonoraszallies@gmail.com>
2026-06-26 14:11:03 +1200

    5

--- pages/home.py
+++ pages/home.py
@@ -6,13 +6,16 @@ class HomePage:
     root.title("HomePage")
     root.geometry("324x720")
 
-    label = tk.Label(root, text = "Ready to organize your week?", font=('Arial', 16))
-    label.pack()
+    frame = tk.Frame (root,bg="blue",bd=5)
+    frame.pack(padx=10,pady=20)
+
+    label = tk.Label(frame, text = "Ready to organize your week?", font=('Arial', 16))
+    label.pack(pady=(20,0))
 
     def openRecipes():
         os.recipes
-    button=tk.Button(root, text = "Recipe Book", command = openRecipes)
-    button.pack()
+    button=tk.Button(frame, text = "Recipe Book", command = openRecipes)
+    button.pack(pady=(20,0))
 
 
     root.mainloop()
