b9a11e049652f2df933704b531b78dc7743ffb93
Leonora Szallies <leonoraszallies@gmail.com>
2026-07-21 10:20:14 +1200

    4

--- pages/home.py
+++ pages/home.py
@@ -1,21 +1,62 @@
 import tkinter as tk
-import os
-
-class HomePage:
-    root = tk.Tk()
-    root.title("HomePage")
-    root.geometry("324x720")
-
-    frame = tk.Frame (root,bg="blue",bd=5)
-    frame.pack(padx=10,pady=20)
-
-    label = tk.Label(frame, text = "Ready to organize your week?", font=('Arial', 16))
-    label.pack(pady=(20,0))
-
-    def openRecipes():
-        os.recipes
-    button=tk.Button(frame, text = "Recipe Book", command = openRecipes)
-    button.pack(pady=(20,0))
 
 
-    root.mainloop()
+class MyApp(tk.Frame):
+
+    def __init__(self, root):
+
+        self.colour1 = '#222448'
+        self.colour2 = '#54527E'
+        self.colour3 = 'WHITE'
+
+        super().__init__(
+            root, bg=self.colour1        # frame.pack(padx=10,pady=20)
+        )
+
+
+        def load_main_widgets(self):
+            self.create_page_container()
+            self.create_pager()
+
+        def create_page_container(self):
+
+            self.page_container = tk.Frame(
+                self.main_frame,
+                background=self.colour1
+            )
+
+            self.main_frame = self
+            self.main_frame.pack(fill=tk.BOTH, expand=True)
+            self.main_frame.columnconfigure(0, weight=1)
+            self.main_frame.rowconfigure(0, weight=1)
+            self.main_frame.rowconfigure(1, weight=1)
+
+            self.page_container.grid(column=0, row=0, sticky=tk.NSEW)
+
+
+
+        def create_page_container(self):
+            pass
+
+        def create_pager(self):
+
+            self.pager = tk.Frame(
+                self.main_frame,
+                background=self.colour1,
+                height=125,
+                width=400
+            )
+
+        self.pager.columnconfigure(1, weight=1)
+        self.pager.rowconfigure(0, weight=1)
+        self.pager.grid(column=0, row=1, sticky=tk.NS)
+        self.pager.grid_propagate(0)
+
+
+
+root = tk.Tk()
+root.title("MyApp")
+root.geometry("700x500")
+root.resizable(width=False, height=False)
+my_app_instance = MyApp(root)
+root.mainloop()
